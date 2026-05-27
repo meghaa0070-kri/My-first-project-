2e86b53afbb81218f4506d45d6d60cb8f0a58334
megha <meghaa0070@gmail.com>
2026-05-27 17:32:27 +0530

    Add program to check if a number is even or odd

---
+++ CppPrograms/even odd.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter n :";
+    cin>>n;
+    if(n%2==0){
+       cout<<"even number";
+ }
+    else{
+       cout<<"odd number";
+ }
+}
+  
