8a3cf6fb0d5a7e1f8b590a45f1c633d9740c08b5
megha <meghaa0070@gmail.com>
2026-05-28 20:00:31 +0530

    Add program to check if a number is three digits

---
+++ CppPrograms/Logical && operator.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter n : ";
+    cin>>n;
+    if(n>99 && n<1000){
+        cout<<"3 Digit Number";
+    }
+    else{
+        cout<<"Not a 3 digit number";
+    }
+}
