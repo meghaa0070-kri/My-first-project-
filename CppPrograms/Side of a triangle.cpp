d6c58a037cdcbd7ed3795db0f33f0870606d0dfd
megha <meghaa0070@gmail.com>
2026-05-28 20:07:06 +0530

    Add program to validate triangle sides

---
+++ CppPrograms/Side of a triangle.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b,c;
+    cout<<"enter 1st side : ";
+    cin>>a;
+    cout<<"enter 2nd side : ";
+    cin>>b;
+    cout<<"enter 3rd side : ";
+    cin>>c;
+    if((a+b)>c && (b+c)>a && (c+a)>b){
+        cout<<"Valid Triangle";
+    }
+    else{
+        cout<<"Invalid Triangle";
+    }
+
+
+}
